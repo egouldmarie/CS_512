b6f146738b10468db05a24d10e3b19e21f4c7586
egouldmarie <e.gould.marie@gmail.com>
2026-09-10 13:35:06 -0600

    putting in structure

---
+++ homework1/triangle.js
@@ -0,0 +1,21 @@
+// --- Triangle ---
+const sideA = 1.;
+const sideC = sideA/Math.tan(60*(Math.PI/180));
+const sideB = -sideC;
+const positions = new Float32Array([
+    0.0, sideA, 0.0, // vertex 1
+    sideB, 0.0, 0.0, // vertex 2
+    sideC, 0.0, 0.0 // vertex 3
+]);
+
+const colors = new Float32Array([
+    1.0, 0.0, 0.0, // Red
+    0.0, 1.0, 0.0, // Green
+    0.0, 0.0, 1.0 // Blue
+]);
+
+const indices = new Float32Array([
+    0, // vertex 1
+    1, // vertex 2
+    2 // vertex 3
+]);
